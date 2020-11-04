33743b5ba18acc28a7234f71639d3b62bdd67382
Kangkai Ye <yek@MacBook-Pro.local>
2020-11-04 18:10:15 +0100

    Norme + headers

--- src/io/ft_putchar_fd.c
+++ src/io/ft_putchar_fd.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 16:56:43 by kaye              #+#    #+#             */
+/*   Updated: 2020/11/04 17:41:27 by kaye             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 /*
- ** Outputs the character c to given file descriptor.
+** Outputs the character c to given file descriptor.
 */
 
-void ft_putchar_fd(char c, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
-    write(fd, &c, 1);
+	write(fd, &c, 1);
 }
--- src/io/ft_putnbr_fd.c
+++ src/io/ft_putnbr_fd.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 16:56:53 by kaye              #+#    #+#             */
+/*   Updated: 2020/11/04 17:41:32 by kaye             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 /*
- ** Outputs the integer n to given file descriptor.
+** Outputs the integer n to given file descriptor.
 */
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-    unsigned int nbr;
+	unsigned int nbr;
 
-    if (n < 0)
-    {
-        ft_putchar_fd('-', fd);
-        nbr = -(unsigned int)n;
-    }
-    else
-        nbr = (unsigned int)n;
-    if (nbr > 9)
-        ft_putnbr_fd(nbr / 10, fd);
-    ft_putchar_fd(nbr % 10 + '0', fd);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = -(unsigned int)n;
+	}
+	else
+		nbr = (unsigned int)n;
+	if (nbr > 9)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd(nbr % 10 + '0', fd);
 }
