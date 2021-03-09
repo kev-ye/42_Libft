187ea3a8ad4479eb7764a84db9c53f6a38ffe229
Kangkai Ye <yek@MacBook-Pro.local>
2021-03-09 21:09:15 +0100

    Readme

--- src/string/ft_itoa_base.c
+++ src/string/ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 20:55:23 by kaye              #+#    #+#             */
-/*   Updated: 2021/03/09 19:51:04 by kaye             ###   ########.fr       */
+/*   Updated: 2021/03/09 21:09:00 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,13 @@ char	*ft_itoa_base(int n, int base)
 	char			*str;
 	unsigned int	n_tmp;
 
+	n_tmp = n;
+	len = ft_intlen_base(n_tmp, base);
 	if (n < 0)
+	{
 		n_tmp = -(unsigned int)n;
-	else
-		n_tmp = n;
-	if (n < 0)
 		len = ft_intlen_base(n_tmp, base) + 1;
-	else
-		len = ft_intlen_base(n_tmp, base);
+	}
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
